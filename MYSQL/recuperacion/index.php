99420ec9d0f7423f85e53541bbae1bcd381e6ac5
rauldedie <rauldedie@gmail.com>
2023-04-09 11:12:16 +0200

    ok

--- MYSQL/recuperacion/index.php
+++ MYSQL/recuperacion/index.php
@@ -1,11 +1,11 @@
 
 <?php   
     header("content-type:text/html;charset=utf-8");
-    include (cabecera.php);
+    include ("cabecera.php");
     if (isset($_POST["registro"]))
     {
         //¿PODRIA SACARLO DEL IF? EL INCLUDE Y LA TOMA DE DATOS
-        include(conectar.php);
+        include("conectar.php");
         
         $nombre =mysqli_real_escape_string($enlace,$_POST["nombre"]);
         $apellidos =mysqli_real_escape_string($enlace,$_POST["apellidos"]);
@@ -16,36 +16,42 @@
         $nuevopass2 = mysqli_real_escape_string($enlace,$_POST["pass2"]);
 
         //VERIFICAR QUE NO EXISTE EL USUARIO
-        $query = sprintf("SELECT nombreusuario FROM usuarios2 WHERE nombreusuario='%s' limit 1",$nuevousu);
+        $query = sprintf("SELECT * FROM usuarios2 WHERE nombreusuario='%s'",$nuevousu);
         $resultado = mysqli_query($enlace,$query);
 
-        if ($resultado)
+        if (!$resultado)
         {
-            echo "El usuario ya existe";
-            //echo "<script> document.getElementById("aviso").innerHTML="El usuario ya existe"; </script>";
+            $query = "SELECT last_insert_id();";
+            $id = mysqli_query($enlace,$query);
+            $id+=1;
+            $passh= md5(md5($id).$nuevopass);
 
-        }else
-        {        
-            $query = "insert into usuarios2 (idusuario,nombre,apellidos,mail,password,nombreusuario) values ('','".$nuevousu."','".$nuevopass."');";
+            $query = "insert into usuarios2 (idusuario,nombre,apellidos,mail,password,nombreusuario) values ('','".$nuevousu."','".$passh."');";
             $resultado = mysqli_query($enlace,$query);
 
             if ($resultado)
             {
                 echo "Te has dado de alta correcctamente.";
-                include(enviarmail.php);
+                include("enviarmail.php");
                 //echo "<script> document.getElementById("aviso").innerHTML="Mail registro enviado"; </script>";
     
             }else
             {
                 echo "Lo siento, ha ocurrido un error en el proceso de alta<br>" . mysqli_error($enlace);
                 //echo "<script> document.getElementById("aviso").innerHTML="Lo siento, ha ocurrido un error en el proceso de alta"; </script>";
-            }    
+            } 
+            
+        }else
+        {        
+            echo "El usuario ya existe";
+            //echo "<script> document.getElementById("aviso").innerHTML="El usuario ya existe"; </script>";
+               
         }
 
         if (isset($_POST["login"]))
         {
             //¿PODRIA SACARLO DEL IF? EL INCLUDE Y LA TOMA DE DATOS
-            include(conectar.php);
+            include("conectar.php");
     
             $nombre =mysqli_real_escape_string($enlace,$_POST["nombre"]);
             $apellidos =mysqli_real_escape_string($enlace,$_POST["apellidos"]);
@@ -56,18 +62,33 @@
             
 
             //VER SI EXISTE EL USUARIO
-            $query = sprintf("SELECT nombreusuario FROM usuarios2 WHERE nombreusuario='%s' AND passwword='%s' limit 1",$usu,$pass);
+            $query = sprintf("SELECT * FROM usuarios2 WHERE nombreusuario='%s'",$usu);
             $resultado = mysqli_query($enlace,$query);
 
-            if ($resultado)
+            if (!$resultado)
             {
-                echo "El usuario se ha logado correctamente";
-                //echo "<script> document.getElementById("aviso").innerHTML="Login correcto"; </script>";
+                
+                echo "Lo siento el usuario y/o la contraseña son incorrectos.";
+                //echo "<script> document.getElementById("aviso").innerHTML="Lo siento el usuario y/o la contraseña son incorrectos."; </script>";
+                
 
             }else
             {
-                echo "Lo siento el usuario no existe";
-                //echo "<script> document.getElementById("aviso").innerHTML="El usuario no existe."; </script>";
+                $fila = mysqli_fetch_array ($resultado);
+                $passh= md5(md5($fila["id"]).$pass);
+                if ($passh==$fila["password"])
+                {
+                    echo "Bienvenido ". $fila["nombreusuario"];
+                    echo "El usuario se ha logado correctamente";
+                    //echo "<script> document.getElementById("aviso").innerHTML="Login correcto"; </script>";
+                }else
+                {
+                    echo "Lo siento, error al logarse usuario y/o contraseñas incorrecto.";
+                    //echo "<script> document.getElementById("aviso").innerHTML="Lo siento, error al logarse usuario y/o contraseñas incorrecto."; </script>";
+                    
+                }
+                
+                
             }
         }
 
