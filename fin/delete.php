09b4317df4c4d19724c20c0db777e18a3ebec00d
rauldedie <rauldedie@gmail.com>
2023-05-19 23:18:06 +0200

    ok

--- fin/delete.php
+++ fin/delete.php
@@ -1,12 +1,12 @@
 <?php include "header.php" ?>
 <?php 
-     if(isset($_GET['eliminar']))
-     {
-         $id= htmlspecialchars($_GET['eliminar']);
-         $query = "DELETE FROM incidencias WHERE id = {$id}"; 
-         $delete_query= mysqli_query($conn, $query);
-         // header("Location: home.php");
-         echo "<script>window.location='paneladmin.php';</script>";
-     }
+    if(isset($_GET['eliminar']))
+    {
+        $id= htmlspecialchars($_GET['eliminar']);
+        $query = "DELETE FROM incidencias WHERE id = {$id}"; 
+        $delete_query= mysqli_query($conn, $query);
+        // header("Location: home.php");
+        echo "<script>window.location='paneladmin.php';</script>";
+    }
 ?>
 <?php include "pie.php" ?>
--- fin/versesion.php
+++ fin/versesion.php
@@ -1,32 +1,30 @@
 <?php
-  session_start();
-  $error="";
-  if(array_key_exists('Logout',$_GET))
-  {
-    //viene de algun logout. Limpiamos todas las sesiones
-    session_unset();
-    //evitar que me usen la cookie para recuperar la sesion que he limpiado
-    setcookie('id',"",time()-60*60,true,true);
-    $_COOKIE['id']="";
-    $_COOKIE['rol']="";
-  }
-  else if ((array_key_exists('id',$_SESSION) AND $_SESSION['id']) OR (array_key_exists('id',$_COOKIE) AND $_COOKIE['id']))
-  {
-    //ya tendría la sesion iniciada y no hacemos que se autentique de nuevo
-    //redireccionamos al panel que el corresponde.
-    if ($_COOKIE['rol']=="administrador")
+    session_start();
+    $error="";
+    if(array_key_exists('Logout',$_GET))
     {
-        header("Location:paneladmin.php");
+        //viene de algun logout. Limpiamos todas las sesiones
+        session_unset();
+        //evitar que me usen la cookie para recuperar la sesion que he limpiado
+        setcookie('id',"",time()-60*60,true,true);
+        $_COOKIE['id']="";
+        $_COOKIE['rol']="";
     }
-    if ($_COOKIE['rol']=="direccion")
+    else if ((array_key_exists('id',$_SESSION) AND $_SESSION['id']) OR (array_key_exists('id',$_COOKIE) AND $_COOKIE['id']))
     {
-        header("Location:paneldirec.php");
+        //ya tendría la sesion iniciada y no hacemos que se autentique de nuevo
+        //redireccionamos al panel que el corresponde.
+        if ($_COOKIE['rol']=="administrador")
+        {
+            header("Location:paneladmin.php");
+        }
+        if ($_COOKIE['rol']=="direccion")
+        {
+            header("Location:paneldirec.php");
+        }
+        if ($_COOKIE['rol']=="profesorado")
+        {
+            header("Location:panelprofe.php");
+        }
     }
-    if ($_COOKIE['rol']=="profesorado")
-    {
-        header("Location:panelprofe.php");
-    }
-    
-        
-  }
-  ?>
+?>
